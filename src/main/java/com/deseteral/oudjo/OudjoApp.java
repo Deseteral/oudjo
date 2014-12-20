2c1f293b845777bce05df4885bb9472ca756c250
Deseteral <deseteral@gmail.com>
2014-12-21 00:30:43 +0100

    Basic database skeleton

--- src/main/java/com/deseteral/oudjo/OudjoApp.java
+++ src/main/java/com/deseteral/oudjo/OudjoApp.java
@@ -8,6 +8,8 @@ import javafx.stage.Stage;
 
 public class OudjoApp extends Application {
 
+    static Database database;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
--- src/main/java/com/deseteral/oudjo/WindowController.java
+++ src/main/java/com/deseteral/oudjo/WindowController.java
@@ -1,4 +1,17 @@
 package com.deseteral.oudjo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class WindowController {
+
+    @FXML
+    private TextField txtDatabasePath;
+
+    public void buttonStartPressed(ActionEvent event) {
+
+        String path = txtDatabasePath.getText();
+        OudjoApp.database = new Database(path);
+    }
 }

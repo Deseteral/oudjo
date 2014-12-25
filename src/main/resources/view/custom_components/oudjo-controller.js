2519c48c0599f65b91c2d209c2b54cfcf4b36374
Deseteral <deseteral@gmail.com>
2014-12-26 00:26:34 +0100

    Code cleanup

--- src/main/resources/view/custom_components/oudjo-controller.js
+++ src/main/resources/view/custom_components/oudjo-controller.js
@@ -1,29 +1,28 @@
 Polymer({
     ready: function() {
+
+        var self = this;
+
+        this.$["button-previous"].onclick = function() {
+            $.get("/player/previous", function(data, status) {
+                self.updateSongInfo(self, JSON.parse(data));
+            });
+        };
+
+        this.$["button-play"].onclick = function() {
+            $.get("/player/play", function(data, status) {
+                self.updateSongInfo(self, JSON.parse(data));
+            });
+        };
+
+        this.$["button-next"].onclick = function() {
+            $.get("/player/next", function(data, status) {
+                self.updateSongInfo(self, JSON.parse(data));
+            });
+        };
     },
     updateSongInfo: function(self, songInfo) {
         self.$["song-title"].innerHTML = songInfo.currentSong.title;
         self.$["song-artist"].innerHTML = songInfo.currentSong.artist;
-    },
-    onPreviousPress: function() {
-        var self = this;
-
-        $.get("/player/previous", function(data, status) {
-            self.updateSongInfo(self, JSON.parse(data));
-        });
-    },
-    onPlayPress: function() {
-        var self = this;
-
-        $.get("/player/play", function(data, status) {
-            self.updateSongInfo(self, JSON.parse(data));
-        });
-    },
-    onNextPress: function() {
-        var self = this;
-
-        $.get("/player/next", function(data, status) {
-            self.updateSongInfo(self, JSON.parse(data));
-        });
     }
 });

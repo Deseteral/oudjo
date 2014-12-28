df124d98402b21bcef2027558b1d34926e622f74
Deseteral <deseteral@gmail.com>
2014-12-28 16:36:47 +0100

    Changing volume changes mute button icon

--- src/main/resources/view/custom_components/oudjo-controller.js
+++ src/main/resources/view/custom_components/oudjo-controller.js
@@ -25,7 +25,7 @@ Polymer({
 
             $.post("/player/volume/" + vol, function(data) {
                 var status = JSON.parse(data);
-                self.$["volume-slider"].value = status.volume;
+                self.updateVolume(self, status.volume);
             });
         });
 
@@ -36,8 +36,6 @@ Polymer({
         }, 500);
     },
     updateStatus: function(self, status) {
-
-        console.log(status);
 
         if (self.playerStatus === undefined ||
             self.playerStatus.currentSong.id != status.currentSong.id) {
@@ -55,6 +53,18 @@ Polymer({
             else
                 self.$["play-icon"].icon = "av:play-arrow";
         }
+    },
+    updateVolume: function(self, vol) {
+        self.$["volume-slider"].value = vol;
+
+        if (vol === 0)
+            self.$["button-mute"].icon = "av:volume-off";
+        else if (vol > 0 && vol < 33)
+            self.$["button-mute"].icon = "av:volume-mute";
+        else if (vol >= 33 && vol < 66)
+            self.$["button-mute"].icon = "av:volume-down";
+        else if (vol >= 66)
+            self.$["button-mute"].icon = "av:volume-up";
     },
     playerStatus: undefined
 });

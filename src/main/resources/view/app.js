500c01323a3a1035741c3c71966d5a3d9682ec79
Deseteral <deseteral@gmail.com>
2015-01-26 21:16:24 +0100

    Code cleanup and documentation

--- src/main/resources/view/app.js
+++ src/main/resources/view/app.js
@@ -1,20 +1,26 @@
 window.addEventListener("polymer-ready", function(e) {
 
-    $("#navicon")[0].addEventListener("click", function() {
-        $("#drawer-panel")[0].togglePanel();
+    var navicon = $("#navicon")[0];
+    var drawerPanel = $("#drawer-panel")[0];
+    var oudjoController = $("oudjo-controller")[0];
+    var controllerDrawer = $("#controller-drawer")[0];
+
+    navicon.addEventListener("click", function() {
+        drawerPanel.togglePanel();
     });
 
-    $("oudjo-controller")[0].addEventListener("volume-slider-touch", function(e) {
+    oudjoController.addEventListener("volume-slider-touch", function(e) {
 
-        if (e.detail)
-            $("#controller-drawer")[0].disableSwipe = true;
-        else
-            $("#controller-drawer")[0].disableSwipe = false;
+        if (e.detail) {
+            controllerDrawer.disableSwipe = true;
+        } else {
+            controllerDrawer.disableSwipe = false;
+        }
     });
 
-    $("#controller-drawer")[0].addEventListener("core-select", function() {
-        $("#controller-drawer::shadow core-selector #drawer")[0].style.boxShadow =
-            "0px 0px 0px 0px rgba(0,0,0,0)";
+    controllerDrawer.addEventListener("core-select", function() {
+        $("#controller-drawer::shadow core-selector #drawer")[0]
+            .style.boxShadow = "0px 0px 0px 0px rgba(0,0,0,0)";
     });
 
     hideFakeToolbar();
@@ -24,13 +30,21 @@ $(window).resize(function() {
     hideFakeToolbar();
 });
 
+/*
+ * Check if fake toolbar above oudjo-controller needs to be hidden.
+ *
+ * When controller drawer is not in narrow mode (e.g. on desktop), opacity of fake toolbar
+ *   must be set to 100%.
+ * When controller drawer is narrow (e.g. on smartphones), opacity of fake toolbar must be
+ *   set to 0%.
+ */
 function hideFakeToolbar() {
-    var drawer = $("#controller-drawer");
-    var fakeToolbar = $("#controller-drawer div[drawer] core-toolbar");
+    var drawer = $("#controller-drawer")[0];
+    var fakeToolbar = $("#controller-drawer div[drawer] core-toolbar")[0];
 
-    if (drawer[0].narrow) {
-        fakeToolbar[0].style.opacity = "0.0";
+    if (drawer.narrow) {
+        fakeToolbar.style.opacity = "0.0";
     } else {
-        fakeToolbar[0].style.opacity = "1.0";
+        fakeToolbar.style.opacity = "1.0";
     }
 }

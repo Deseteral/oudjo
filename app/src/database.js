811240e3be500df62396490bb92ad37908647f31
Deseteral <deseteral@gmail.com>
2015-05-27 02:00:18 +0200

    Completed scanning implementation

--- app/src/database.js
+++ app/src/database.js
@@ -1,12 +1,17 @@
 var fs = require('fs');
 var Datastore = require('nedb');
-var recursiveReaddir = require('recursive-readdir');
 var path = require('path');
+var mm = require('musicmetadata');
+var walk = require('walk');
+var Song = require('./song');
 
 function Database() {
   this.isOpen = false;
   this.path = null;
-  this.db = {};
+
+  this.library = null;
+  this.albums = null;
+  this.artists = null;
 }
 
 Database.prototype.open = function(path) {
@@ -24,9 +29,9 @@ Database.prototype.open = function(path) {
   }
 
   this.path = path;
-  this.db.library = new Datastore(path + '/.oudjo/library.db');
-  this.db.albums = new Datastore(path + '/.oudjo/albums.db');
-  this.db.artists = new Datastore(path + '/.oudjo/artists.db');
+  this.library = new Datastore(path + '/.oudjo/library.db');
+  this.albums = new Datastore(path + '/.oudjo/albums.db');
+  this.artists = new Datastore(path + '/.oudjo/artists.db');
 
   var databasesToLoad = 3;
   var onDatabaseError = function(err, dbname) {
@@ -43,15 +48,15 @@ Database.prototype.open = function(path) {
     }
   }.bind(this);
 
-  this.db.library.loadDatabase(function(err) {
+  this.library.loadDatabase(function(err) {
     onDatabaseError(err, 'library');
   });
 
-  this.db.albums.loadDatabase(function(err) {
+  this.albums.loadDatabase(function(err) {
     onDatabaseError(err, 'albums');
   });
 
-  this.db.artists.loadDatabase(function(err) {
+  this.artists.loadDatabase(function(err) {
     onDatabaseError(err, 'artists');
   });
 };
@@ -61,16 +66,113 @@ Database.prototype.scan = function() {
     throw new Error('Couldn\'t scan database, because it isn\'t open');
   }
 
-  recursiveReaddir(this.path, function(err, files) {
-
-    files.forEach(function(filePath) {
-      var extension = path.extname(filePath).toLowerCase();
-
-      if (extension !== '.mp3') {
-        return;
+  // Count the amount of files in the directory
+  var fileCount = 0;
+  var walkOptions = {
+    listeners: {
+      names: function(root, nodeNamesArray) {
+        fileCount += nodeNamesArray.length;
       }
-    });
-  });
+    }
+  };
+
+  walk.walkSync(this.path, walkOptions);
+  console.log(`Files to scan: ${fileCount}`);
+
+  // Scan files
+  walkOptions = {
+    listeners: {
+      file: this._scanOnFile.bind(this)
+    }
+  };
+  walk.walk(this.path, walkOptions);
+};
+
+Database.prototype._scanOnFile = function(root, fileStat, next) {
+  var filePath = path.resolve(root, fileStat.name);
+  var extension = path.extname(filePath).toLowerCase();
+
+  if (extension !== '.mp3') {
+    next();
+    return;
+  }
+
+  mm(fs.createReadStream(filePath), function(err, metadata) {
+    if (err) {
+      console.error(err);
+    }
+
+    var song = new Song(
+      metadata.title,
+      metadata.artist[0],
+      metadata.album,
+      metadata.year
+    );
+
+    var addArtistId = function() {
+      return new Promise(function(fulfill, reject) {
+        this.artists.find({ name: song.artist }, function(err, docs) {
+          if (err) {
+            reject(err);
+          }
+
+          if (docs.length === 1) {
+            song.artistId = docs[0]._id;
+            fulfill();
+          } else {
+            this.artists.insert({ name: song.artist }, function(err, newDoc) {
+              if (err) {
+                reject(err);
+              }
+
+              song.artistId = newDoc._id;
+              fulfill();
+            }.bind(this));
+          }
+        }.bind(this));
+      }.bind(this));
+    }.bind(this);
+
+    var addAlbumId = function() {
+      return new Promise(function(fulfill, reject) {
+        this.albums.find({ name: song.album }, function(err, docs) {
+          if (err) {
+            reject(err);
+          }
+
+          if (docs.length === 1) {
+            song.albumId = docs[0]._id;
+            fulfill();
+          } else {
+            this.albums.insert({ name: song.album }, function(err, newDoc) {
+              if (err) {
+                reject(err);
+              }
+
+              song.albumId = newDoc._id;
+              fulfill();
+            }.bind(this));
+          }
+        }.bind(this));
+      }.bind(this));
+    }.bind(this);
+
+    var insertSong = function() {
+      this.library.insert(song, function(err) {
+        if (err) {
+          console.error(err);
+        }
+
+        next();
+      });
+    }.bind(this);
+
+    addArtistId()
+      .then(addAlbumId)
+      .then(insertSong)
+      .catch(console.error);
+
+  }.bind(this));
 };
 
 module.exports = Database;
---
+++ app/src/song.js
@@ -0,0 +1,11 @@
+
+function Song(title, artist, album, year) {
+  this.title = title;
+  this.artistId = null;
+  this.artist = artist;
+  this.albumId = null;
+  this.album = album;
+  this.year = year;
+}
+
+module.exports = Song;

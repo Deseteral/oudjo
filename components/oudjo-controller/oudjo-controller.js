d32c3bb55e0040ab1744ecb48ed03bc89cfe29a2
Deseteral <deseteral@gmail.com>
2015-06-05 18:28:57 +0200

    Small album art fix

--- components/oudjo-controller/oudjo-controller.js
+++ components/oudjo-controller/oudjo-controller.js
@@ -60,14 +60,16 @@ Polymer({
     if (status.song === undefined) {
       this.songTitle = 'oudjo';
       this.songArtist = '';
+
+      this.$['album-art'].songId = null;
     } else if (status.song._id !== this._lastSongId) { // If the song has changed
       this.songTitle = status.song.title;
       this.songArtist = status.song.artist;
 
+      this.$['album-art'].songId = status.song._id;
+
       this._lastSongId = status.song._id;
     }
-
-    this.$['album-art'].songId = status.song._id;
 
     if (status.isPaused) {
       this.$['icon-play'].icon = 'av:play-arrow';

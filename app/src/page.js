e6b53e96cc71c015663843c29bca42cd3f3272d1
Deseteral <deseteral@gmail.com>
2015-06-16 21:37:05 +0200

    Drawer overhaul

--- app/src/page.js
+++ app/src/page.js
@@ -5,4 +5,15 @@ window.addEventListener('WebComponentsReady', function() {
   console.log('%coudjo -- main-window', 'font-size: x-large; background: ' +
     '-webkit-linear-gradient(top, #bcff05 0%,#70cd19 100%); -webkit-background-clip: ' +
     'text; -webkit-text-fill-color: transparent;');
+
+  var app = document.querySelector('#app');
+  app.page = 'library';
+
+  var drawerMenu = document.querySelector('#drawer-menu');
+  var drawerPanel = document.querySelector('paper-drawer-panel');
+
+  // Close the drawer, when item is selected
+  drawerMenu.addEventListener('iron-activate', function() {
+    drawerPanel.closeDrawer();
+  });
 });

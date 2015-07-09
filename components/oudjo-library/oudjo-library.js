3f4a6504ef807798670fda77369cd45bc80da36a
Michał Ferchow <deseteral@gmail.com>
2015-07-09 13:29:34 +0200

    oudjo-library fetches library data

--- components/oudjo-library/oudjo-library.js
+++ components/oudjo-library/oudjo-library.js
@@ -1,7 +1,20 @@
 Polymer({
   is: 'oudjo-library',
 
+  properties: {
+    library: {
+      type: Array
+    }
+  },
+
   ready: function() {
     this.tab = 'songs';
+
+    fetch('/library')
+      .then(function(response) {
+        return response.json();
+      }).then(function(json) {
+        this.library = json;
+      }.bind(this));
   }
 });

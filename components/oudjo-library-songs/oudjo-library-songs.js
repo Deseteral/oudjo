cb664b1a6505625faaa570dcd163ecaabe3b34f5
Michał Ferchow <deseteral@gmail.com>
2015-07-21 23:14:41 +0200

    This is not working, and I'm fully aware of this.

--- components/oudjo-library-songs/oudjo-library-songs.js
+++ components/oudjo-library-songs/oudjo-library-songs.js
@@ -1,3 +1,23 @@
 Polymer({
-  is: 'oudjo-library-songs'
+  is: 'oudjo-library-songs',
+
+  properties: {
+    librarySource: {
+      type: Array
+    }
+  },
+
+  ready: function() {
+
+    this.librarySource = [
+      { title: 'title1', artist: 'artist1' },
+      { title: 'title2', artist: 'artist2' },
+      { title: 'title3', artist: 'artist3' },
+      { title: 'title4', artist: 'artist4' }
+    ];
+
+    // window.setTimeout(function() {
+    //   this.librarySource = window.document.querySelector('oudjo-library').library;
+    // }, 1000);
+  }
 });

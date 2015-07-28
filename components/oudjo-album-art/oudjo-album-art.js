8dc23ca9ad598420f824d563b2f37ed61d5f40f7
Deseteral <deseteral@gmail.com>
2015-07-28 18:52:57 +0200

    Fixed wrong album art rendering

--- components/oudjo-album-art/oudjo-album-art.js
+++ components/oudjo-album-art/oudjo-album-art.js
@@ -6,16 +6,24 @@ Polymer({
       type: String,
       reflectToAttribute: true,
       observer: '_songIdChanged'
+    },
+    tweenArts: {
+      type: Boolean,
+      value: false
     }
   },
 
   _songIdChanged: function(newValue) {
-    if (newValue !== undefined && newValue !== '' && newValue !== null) {
-      this.$['album-art-image'].placeholder = this.$['album-art-image'].src;
-      this.$['album-art-image'].src = '/library/' + newValue + '/art';
+    if (this.tweenArts) {
+      if (newValue !== undefined && newValue !== '' && newValue !== null) {
+        this.$['album-art-image'].placeholder = this.$['album-art-image'].src;
+        this.$['album-art-image'].src = '/library/' + newValue + '/art';
+      } else {
+        this.$['album-art-image'].placeholder = this.$['album-art-image'].src;
+        this.$['album-art-image'].src = '../../resources/oudjo-album-art.png';
+      }
     } else {
-      this.$['album-art-image'].placeholder = this.$['album-art-image'].src;
-      this.$['album-art-image'].src = '../../resources/oudjo-album-art.png';
+      this.$['album-art-image'].src = '/library/' + newValue + '/art';
     }
   }
 });

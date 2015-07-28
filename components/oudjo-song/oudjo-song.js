bbd6f04374af7188dc70b764a79af24e5e6b5b2b
Deseteral <deseteral@gmail.com>
2015-07-28 22:08:30 +0200

    Correctly resizing oudjo-song

--- components/oudjo-song/oudjo-song.js
+++ components/oudjo-song/oudjo-song.js
@@ -12,16 +12,32 @@ Polymer({
 
   ready: function() {
     window.onResizeHandler.push(this._onResize.bind(this));
-    this._onResize();
+
+    // Give some time for the element to load the data
+    window.setTimeout(function() {
+      this._onResize();
+    }.bind(this), 100);
   },
 
   _onResize: function() {
-    var windowWidth = window.innerWidth;
+    var width = this.$.container.clientWidth;
 
-    if (windowWidth <= 400) {
-      this.$['info-year'].style.display = 'none';
+    if (width <= 700) {
+      this.$['info-length'].style.display = 'none';
     } else {
-      this.$['info-year'].style.display = '';
+      this.$['info-length'].style.display = '';
+    }
+
+    if (width <= 550) {
+      this.$['year-container'].style.display = 'none';
+    } else {
+      this.$['year-container'].style.display = '';
+    }
+
+    if (width <= 500) {
+      this.$['info-album'].style.display = 'none';
+    } else {
+      this.$['info-album'].style.display = '';
     }
   }
 });

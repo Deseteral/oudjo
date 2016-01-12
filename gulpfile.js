b4e0c4bf5361ffacb91d6ab2c6c8e61786ad17b8
Deseteral <deseteral@gmail.com>
2016-01-12 13:17:15 +0100

    Fixed issue with build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const electron = require('gulp-run-electron');
 
-gulp.task('default', () => {
-  return gulp.src('src/**/*.js')
+gulp.task('default', ['build-js', 'build-html'], () => {
+  return gulp
+    .src('.')
+    .pipe(electron());
+});
+
+gulp.task('build-js', () => {
+  return gulp
+    .src('src/**/*.js')
+    .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015']
     }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build-html', () => {
+  return gulp
+    .src('src/**/*.html')
     .pipe(gulp.dest('build'));
 });

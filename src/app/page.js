a82d05a49ca286c9cb31eb0f677cd8e4d94ddbc7
Deseteral <deseteral@gmail.com>
2016-01-12 13:23:28 +0100

    Added pretty info into dev tools console :P

---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+// Print pretty info into the console
+console.log('%coudjo -- main-window', 'font-size: x-large; background: ' +
+  '-webkit-linear-gradient(top, #bcff05 0%,#70cd19 100%);' +
+  '-webkit-background-clip: text;' +
+  '-webkit-text-fill-color: transparent;');
---
+++ src/core.js
@@ -0,0 +1,5 @@
+// Print pretty info into the console
+console.log('%coudjo -- core', 'font-size: x-large; background: ' +
+  '-webkit-linear-gradient(top, #ffbbed 0%,#ff4da0 100%);' +
+  '-webkit-background-clip: text;' +
+  '-webkit-text-fill-color: transparent;');
